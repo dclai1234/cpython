c00fe3e4303ff284c5619a939fcc0770e2345da5
dclai1234 <dsssclai@gmail.com>
2024-07-06 00:30:02 +0530

    a1__main__.py

--- Lib/idlelib/__main__.py
+++ Lib/idlelib/__main__.py
@@ -1,8 +1,24 @@
-"""
-IDLE main entry point
+def seconds_difference(time1, time2):
+    return time2 - time1
 
-Run IDLE as python -m idlelib
-"""
-import idlelib.pyshell
-idlelib.pyshell.main()
-# This file does not work for 2.7; See issue 24212.
+def hours_difference(time1, time2):
+    return (time2 - time1) / 3600
+
+def to_float_hours(hours, minutes, seconds):
+    return hours + minutes / 60 + seconds / 3600
+
+def get_hours(seconds):
+    return seconds // 3600
+
+def get_minutes(seconds):
+    return (seconds % 3600) // 60
+
+def get_seconds(seconds):
+    return seconds % 60
+
+def time_to_utc(offset, time):
+    return time - offset
+
+def time_from_utc(offset, time):
+    return time + offset
+
